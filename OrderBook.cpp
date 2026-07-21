52f461919c0b2828e0e0eb3e058ac4734df24e00
Asher Raymon <ashraymon24@gmail.com>
2026-07-21 15:46:41 +0100

    Completed basic order matching function

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <map>
 #include <deque>
+#include <algorithm>
 
 enum class OrderType {
     GoodTillCancel,
@@ -38,14 +39,71 @@ class OrderBook {
     private:
         std::map<Price, std::deque<Order>, std::greater<Price>> bids;
         std::map<Price, std::deque<Order>> asks;
-    public:
-        void addOrder(OrderId id, Quantity quantity, Price price, Side side) {
-            if (side == Side::Buy) {
-                bids[price].emplace_back(id, quantity, price, side);
+
+        void addOrder(const Order& order) {
+            if (order.getSide() == Side::Buy) {
+                bids[order.getPrice()].emplace_back(order.getID(), order.getQuantity(), order.getPrice(), order.getSide());
             } else {
-                asks[price].emplace_back(id, quantity, price, side);
+                asks[order.getPrice()].emplace_back(order.getID(), order.getQuantity(), order.getPrice(), order.getSide());
             }
         }
+        void matchOrder(Order& incomingOrder){
+            if (incomingOrder.getSide() == Side::Buy) {
+                auto it = asks.begin();
+                while (incomingOrder.getQuantity() > 0 && it != asks.end()) {
+                    Price bestPrice = it->first;
+                    if (bestPrice > incomingOrder.getPrice()){
+                        break; // No more matching possible
+                    }
+                    auto& ordersAtPrice = it->second;
+                    while (!ordersAtPrice.empty() && incomingOrder.getQuantity() > 0) {
+                        Order& existingOrder = ordersAtPrice.front();
+                        Quantity matchQuantity = std::min(incomingOrder.getQuantity(), existingOrder.getQuantity());
+                        incomingOrder.setQuantity(incomingOrder.getQuantity() - matchQuantity);
+                        existingOrder.setQuantity(existingOrder.getQuantity() - matchQuantity);
+                        if (existingOrder.getQuantity() == 0) {
+                            ordersAtPrice.pop_front(); // Remove fully matched order
+                        }
+                    }
+                    if (ordersAtPrice.empty()) {
+                        it = asks.erase(it);
+                    } else {
+                        ++it;
+                    }
+                }
+            } else {
+                auto it = bids.begin();
+                while (incomingOrder.getQuantity() > 0 && it != bids.end()) {
+                    Price bestPrice = it->first;
+                    if (bestPrice < incomingOrder.getPrice()) {
+                        break; // No more matching possible
+                    }
+                    auto& ordersAtPrice = it->second;
+                    while (!ordersAtPrice.empty() && incomingOrder.getQuantity() > 0) {
+                        Order& existingOrder = ordersAtPrice.front();
+                        Quantity matchQuantity = std::min(incomingOrder.getQuantity(), existingOrder.getQuantity());
+                        incomingOrder.setQuantity(incomingOrder.getQuantity() - matchQuantity);
+                        existingOrder.setQuantity(existingOrder.getQuantity() - matchQuantity);
+                        if (existingOrder.getQuantity() == 0) {
+                            ordersAtPrice.pop_front(); // Remove fully matched order
+                        }
+                    }
+                    if (ordersAtPrice.empty()) {
+                        it = bids.erase(it);
+                    } else {
+                        ++it;
+                    }
+                };
+            };
+        }
+    public:
+        void processOrder(Order order) {
+            matchOrder(order);
+            if (order.getQuantity() > 0) {
+                addOrder(order);
+            }
+        }
+        
         void printOrders() const {
             printSide(bids);
             printSide(asks);
